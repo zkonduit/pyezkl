25c09964c3c51f3e109480513238886292cf8adc
Jseam <hello.jseam@gmail.com>
2023-05-21 19:05:32 +0800

    feat: add python bindings for `create_evm_verifier` and `verify_evm` (#9)

--- ezkl/__init__.py
+++ ezkl/__init__.py
@@ -1,3 +1,14 @@
 from .export import export
 
-from ezkl_lib import PyRunArgs, table, gen_srs, forward, mock, setup, prove, verify
+from ezkl_lib import (
+    PyRunArgs,
+    table,
+    gen_srs,
+    forward,
+    mock,
+    setup,
+    prove,
+    verify,
+    create_evm_verifier,
+    verify_evm
+)
